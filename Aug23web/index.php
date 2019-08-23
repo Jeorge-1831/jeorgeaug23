a06d0d40d900124a54f1cbde66da047b405f7427
Jeorge-1831 <53499795+Jeorge-1831@users.noreply.github.com>
2019-08-23 19:01:26 +0800

    Update and rename login.html to index.php

--- Aug23web/index.php
+++ Aug23web/index.php
@@ -5,14 +5,14 @@
 
 <h1 align="center" style="color: white">Login</h1>
 <hr>
-<center><form action="list photos.html">
+<center><form action="list photos.php">
   <br>Username:
   <input type="text" name="Username">
   <br><br>Password:
   <input type="Password" name="Password" ">
   <br><br>
   <input type="submit" value="Login">
- <a href="register.html">Register?</a>
+ <a href="register.php">Register?</a>
  <hr>
 </form> </center>
 </body>
